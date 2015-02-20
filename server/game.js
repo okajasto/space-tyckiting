3feaaf36370b94719777eddd06f25cfde0bb93e5
Osmo Kajasto <osmo.kajasto@futurice.com>
2015-02-20 10:19:58 +0200

    Changed socket.io -> websocket

--- server/game.js
+++ server/game.js
@@ -3,6 +3,7 @@ var Messages = require('./messages.js');
 var ActionLog = require('./actionlog.js');
 var _ = require('lodash');
 var loop = require('./gameloop.js');
+var ws = require('ws');
 
 function Game(config) {
 
@@ -28,59 +29,67 @@ function Game(config) {
         return Math.floor(Math.random() * val);
     }
 
-    function startListening(io) {
+    function startListening(ws) {
 
-        io.on('connection', function(socket){
+        ws.on('connection', function(socket){
 
             var id = idCounter++;
             var player = {id: id, socket: socket};
 
             allPlayers.push(player);
 
-            socket.on("action", function(data) {
-                console.log("%s: ", player.name, data);
-                if (!finished) {
-                    player.action = data;
+            socket.on("message", function(rawData) {
+                var data = JSON.parse(rawData);
+                var content = data.data;
+
+                if (data.type === "action") {
+                    console.log("%s: ", player.name, content);
+                    if (!finished) {
+                        player.action = content;
+                    }
+                } else if (data.type === "message") {
+
+                } else if (data.type === "join" ) {
+
+                    if (started) {
+                        if (socket) {
+                            socket.send(JSON.stringify({type: "error", data: "Already started"}));
+                        }
+                        return;
+                    }
+                    // Clear inactive players (players without connection)
+                    allPlayers = _.filter(allPlayers, function(player) {
+                        return player.socket.readyState === player.socket.OPEN;
+                    });
+
+                    console.log("Received join", player.id,  player.socket.readyState);
+
+                    player.name = content.name;
+                    player.team = content.team;
+                    player.active = true;
+
+                    if (Rules.checkForStart(allPlayers, config.teamPlayers)) {
+                        start(_.where(allPlayers, {active: true}), config);
+                    }
                 }
             });
 
-            socket.on("message", function(message) {
+        /*    socket.on("message", function(message) {
                 player.message = message;
-            });
+            }); */
 
-            socket.on('disconnect', function () {
+        /*    socket.on('disconnect', function () {
                 console.log("Disconnect", id);
                 player.active = false;
-            });
+            }); */
 
-            socket.on("join", function(data) {
-                if (started) {
-                    if (socket) {
-                        socket.emit("err", "Already started");
-                    }
-                    return;
-                }
-                // Clear inactive players (players without connection)
-                allPlayers = _.filter(allPlayers, function(player) {
-                    return player.socket;
-                });
-
-                console.log("Received join", player.id, data);
-                player.name = data.name;
-                player.team = data.team;
-                player.active = true;
-
-                if (Rules.checkForStart(allPlayers, config.teamPlayers)) {
-                    start(_.where(allPlayers, {active: true}), config);
-                }
-            });
-            socket.emit("connected", {id: id, config: config});
+            socket.send(JSON.stringify({type: "connected", data: {id: id, config: config}}));
         });
 
         var sendToTeam = function(team, players, eventType, data) {
             var members = _.where(players, {team: team});
             members.forEach(function(member) {
-                member.socket.emit(eventType, data);
+                member.socket.send(JSON.stringify({type: eventType, data: data}));
             });
         };
 
@@ -93,8 +102,8 @@ function Game(config) {
                 player.pos = {x: rand(config.width), y: rand(config.height)};
             });
             players.forEach(function(player) {
-                if (player.socket) {
-                    player.socket.emit("start", Messages.startMessage(player, players, config));
+                if (player.socket && player.socket.readyState === player.socket.OPEN) {
+                    player.socket.send(JSON.stringify({type: "start", data: Messages.startMessage(player, players, config)}));
                 }
             });
             started = true;
@@ -164,107 +173,6 @@ function Game(config) {
                     }));
                 });
             }
-         /*   // Actually just mutates the original array
-            var activePlayers = _.filter(players, function(player) {
-                return player.hp > 0 && player.active;
-            });
-
-            if (counter === 0) {
-                // The round 0 is start round. No actions should be performed before it.
-                activePlayers.forEach(function(player) {
-                    player.action = null;
-                });
-            } else {
-                activePlayers.forEach(function(player) {
-                    if (player.action) {
-                        player.action.x = parseInt(player.action.x, 10);
-                        player.action.y = parseInt(player.action.y, 10);
-                    }
-                });
-            }
-            // MOVE
-            // TODO Handle error cases
-            var moves = Rules.getMoveEvents(activePlayers, config.move, config.width, config.height);
-            activePlayers = Rules.applyMoveEvents(moves, activePlayers);
-            // RADAR includes both detections by radar and seeing range
-            var radars = Rules.getRadarEvents(activePlayers, config.radar);
-            radars = radars.concat(Rules.getSeeingEvents(activePlayers, config.see));
-
-            // CANNONS
-            var cannons = Rules.getCannonEvents(activePlayers, config.cannon);
-
-            // Noactions
-            var noActions = Rules.getNoActions(activePlayers);
-
-            if (counter !== 0) {
-                // Add statistics after move so action results are applied.
-                statistics.turns.push(ActionLog.getTurnActions(activePlayers));
-            }
-
-            activePlayers = Rules.applyDamages(cannons, activePlayers);
-            var isFinished = Rules.isFinished(players, counter, config.maxCount);
-
-            if (isFinished) {
-                finished = true;
-                started = false;
-
-                var endResult = Rules.getCurrentGameStatus(players);
-
-                var message = Messages.endMessage(endResult.winner);
-                console.log("Game Ended", message);
-
-                ActionLog.writeLog(_.pluck(endResult.teamHps, "team"), endResult.winner, statistics);
-
-                players.forEach(function(player) {
-                    if (player.socket) {
-                        player.socket.emit("end", message);
-                    }
-                });
-            } else {
-
-                var broadCasts = [Messages.getRoundStartMessage(counter)];
-
-                var messages = Messages.getMessages(activePlayers);
-                statistics.messages.push({round: counter, messages: messages});
-
-                broadCasts = broadCasts.concat(messages);
-
-                // Check if any tank was destroyed this turn
-                var newlyDestroyed = _.filter(activePlayers, function(player) {
-                    return player.hp <= 0;
-                });
-
-                if (!_.isEmpty(newlyDestroyed)) {
-                    broadCasts = broadCasts.concat(newlyDestroyed.map(Messages.destroyedEventMessage));
-                }
-
-                var teamEvents = _.reduce(teams, function(memo, team) {
-                    var events = [].concat(broadCasts)
-                        .concat(Messages.getMoveMessagesByTeam(moves, players, team))
-                        .concat(Messages.getDetectedMessagesByTeam(radars, players, team))
-                        .concat(Messages.getRadarMessagesByTeam(radars, players, team))
-                        .concat(Messages.getCannonMessagesByTeam(cannons, players, team))
-                        .concat(Messages.getNoActionMessagesByTeam(noActions, team))
-                        .concat(Messages.getTeamStatusMessage(players, team));
-                    memo[team] = events;
-                    return memo;
-                }, {});
-
-
-                players.forEach(function(player) {
-                    // Clear the previous events
-                    player.action = null;
-                    player.message = null;
-                });
-
-                teams.forEach(function(team) {
-                    sendToTeam(team, activePlayers, "events", teamEvents[team]);
-                });
-
-                setTimeout(function () {
-                    gameLoop(teams, players, counter+1, statistics);
-                }, config.loopTime);
-            } */
         };
     };
 
